c39f81dc5adac514f5aa5642565d20a4defac37f
Zane Peckenpaugh <zanepeck01@gmail.com>
2025-12-03 19:31:01 -0500

    added file for image mapping for inforcards for the housing

---
+++ collegeHousingRater/src/main/resources/front-end/react-ui/src/Images/housingImages.js
@@ -0,0 +1,29 @@
+import unitasTowerImage from "./unitas_image.jpeg";
+import belknapNorthImage from "./Belknap_Village_North.webp";
+import belknapSouthImage from "./Belknap_Village_South.webp";
+import bettieJohnsonImage from "./Bettie_Johnson.webp";
+import communityParkImage from "./Community_Park.webp";
+import kurzHallImage from "./Kurz_Hall.webp";
+import louisvilleHallImage from "./Louisville_Hall.webp";
+import cardinalTowneImage from "./Cardinal_Towne.webp";
+import universityPointeImage from "./University_Pointe.jpg";
+import universityTowerAptsImage from "./University_Tower_Apartments.webp";
+import dennyCrumHallImage from "./Denny_Crum_Hall.jpg";
+
+export const housingImages = {
+  // University of Louisville
+  "Unitas Tower": unitasTowerImage,
+  "Belknap Village North": belknapNorthImage,
+  "Belknap Village South": belknapSouthImage,
+  "Bettie Johnson Hall": bettieJohnsonImage,
+  "Community Park": communityParkImage,
+  "Kurz Hall": kurzHallImage,
+  "Louisville Hall": louisvilleHallImage,
+  "Cardinal Towne": cardinalTowneImage,
+  "University Pointe": universityPointeImage,
+  "University Tower Apartments": universityTowerAptsImage,
+  "Denny Crum Hall": dennyCrumHallImage,
+};
+
+// Default image
+export const defaultHousingImage = "https://images.unsplash.com/photo-1555854877-bab0e564b8d5?w=800";
